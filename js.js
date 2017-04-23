0ab637849ee72e48727957b1f6bff69114a1ed73
Hossein <hosseinkf@live.dk>
2017-04-23 06:58:05 -0700

    Revert "media queries"
    This reverts commit a5e6e691ba48878828dab8e25c0d0ab39f5eb9a9.

--- js.js
+++ js.js
@@ -1,5 +1,8 @@
-$(document).ready(function ({
-  $(".Photo img").click(function({
-    $("my-picture").photoZoome();
+$(function functionName() {
+  $(".my-picture").click(function functionName() {
+    var src = $(this).attr("src");
+    $(".frame ").attr("src",src);
+    $(".frame").fadeIn();
+    $(".overlay").fadeIn();
   });
-}));
+});
